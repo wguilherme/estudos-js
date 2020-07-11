371936ece23b2eae7e0e859209ce9ef99f7f57c9
W. Guilherme <ravok123@gmail.com>
2020-07-11 16:19:35 -0300

    [Update] Desafios

---
+++ desafios-samurai/desafio2.1.js
@@ -0,0 +1,17 @@
+var x = "um";
+var y = "dois";
+
+if (x === "um"){
+x = 1;
+}
+
+if (y === "dois"){
+   y = 2;
+}
+
+
+var resultado = x + y;
+
+alert(resultado);
+console.log(resultado);
+
---
+++ desafios-samurai/desafio2.js
@@ -0,0 +1,7 @@
+var x = "5";
+var y = "5";
+var resultado = Number(x) + Number(y);
+
+alert(resultado);
+console.log(resultado);
+
